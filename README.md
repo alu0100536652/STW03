f9db615ee529484e67772b2c359f542d76614aeb
David CR <alu0100536652@ull.edu.es>
2015-10-11 23:35:55 +0000

    Creando gulpfile, css, index y modificando README

--- README.md
+++ README.md
@@ -21,3 +21,7 @@ Esta práctica pertenece a la asignatura Sistemas y Tecnologias Web del grado de
 **Página del autor**
 
 * [Web Author](http://alu0100536652.github.io)
+
+## Datos de desarrollo
+
+Durante la ejecución de tareas gulp se puede presentar un fallo con gulp-notify, para solucionarlo instalar en la terminal unix 'libnotify-bin' con el comando: sudo apt-get install libnotify-bin
--- gulpfile.js
+++ gulpfile.js
@@ -6,4 +6,44 @@ var gulp       = require('gulp'),
     del        = require('del'),
     minifyHTML = require('gulp-minify-html'),
     minifyCSS  = require('gulp-minify-css'),
+    notify     = require('gulp-notify'),
     imagemin   = require('gulp-imagemin');
+    
+gulp.task('Minify', function () {
+
+  gulp.src('./assets/css/*.css')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/css/'))
+   
+  gulp.src('./assets/js/*.js')
+   .pipe(minifyCSS({keepBreaks:true}))
+   .pipe(gulp.dest('./minified/js/'))
+   
+  gulp.src('./index.html')
+    .pipe(minifyHTML())
+    .pipe(gulp.dest('./minified/'))
+    .pipe(notify({ message: 'Minify task complete' }))
+    
+});
+
+gulp.task('Imagemin', function() {
+
+  gulp.src('./assets/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./minified/images/'))
+    .pipe(notify({ message: "Image-Min task complete!"}));
+
+});
+
+gulp.task('Clean', function(cb) {
+  del(['minified/*'], cb)
+});
+
+gulp.task("Test", function(){
+  gulp.src('')
+    .pipe(notify({ message: "Test task complete!"}))
+    .pipe(open({ uri: 'https://alu0100536652.github.io/STW03/test/'}))
+});
+
+gulp.task('default', ['Clean', 'Minify', 'Imagemin', 'Test'], function() {
+});
