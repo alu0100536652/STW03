e837da81d88add85ba6cbd1e8026037d6f3cc10f
David CR <alu0100536652@ull.edu.es>
2015-10-12 11:56:55 +0000

    Haciendo uso de blanket y creando los primeros test para comprobar la cobertura de codigo

--- test/tests.js
+++ test/tests.js
@@ -4,16 +4,29 @@ describe("Conversor", function() {
     
     describe("Constructor", function() {
     
-        it("Valor por defecto", function() {
+        it("Valor por defecto debe ser 0", function() {
           var temperatura = new Temperatura();
           expect(temperatura.valor).to.equal(0);
         });
         
-        it("Tipo por defecto", function() {
+        it("Tipo por defecto debe ser 'Sin tipo.'", function() {
           var temperatura = new Temperatura();
           expect(temperatura.tipo).to.equal(" Sin tipo.");
         });
+        
+        it("Temperatura debe poder recibir dos parametros", function() {
+          var temperatura = new Temperatura(2,'C');
+          expect(temperatura.valor).to.equal(2);
+          expect(temperatura.tipo).to.equal('C');
+        });
     
+    });
+    
+    describe("Metodos",function() {
+        it("GetMedida() debe devolvernos la medida",function(){
+          var temperatura = new Temperatura(2,'C');
+          expect(temperatura.getMedida()).to.equal("2C");
+        });
     });
     
 });
