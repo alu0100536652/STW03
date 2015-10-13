575cc06309af9f75f839e6394d6161feec4c3b4d
David CR <alu0100536652@ull.edu.es>
2015-10-13 16:29:15 +0000

    Solucionando fallo en tests para usar datos del inputs y span, añadiendo y refactorizando tests

--- test/tests.js
+++ test/tests.js
@@ -21,88 +21,129 @@ describe("Conversor Temperatura", function() {
     
   describe("Constructor", function() {
   
-    it("Valor por defecto debe ser 0", function() {
+    it("Instancia de Temperatura() - Valor de una Medida por defecto debe ser 0", function() {
       var temperatura = new Temperatura();
       expect(temperatura.valor).to.equal(0);
     });
     
-    it("Tipo por defecto debe ser 'Sin tipo.'", function() {
-      var temperatura = new Temperatura();
-      expect(temperatura.tipo).to.equal(" Sin tipo.");
+    it("Instancia de Temperatura() - Tipo de una Medida por defecto debe ser 'Sin tipo.'", function() {
+      var distancia = new Distancia();
+      expect(distancia.tipo).to.equal(" Sin tipo.");
     });
     
-    it("Temperatura debe poder recibir dos parametros", function() {
+    it("Instancia de Temperatura() - Temperatura debe poder recibir dos parametros", function() {
       var temperatura = new Temperatura(2,'c');
       expect(temperatura.valor).to.equal(2);
       expect(temperatura.tipo).to.equal('c');
     });
     
-    it("El atributo tipo tiene que ser string", function() {
+    it("Instancia de Temperatura() - El atributo tipo tiene que ser string", function() {
       var temperatura = new Temperatura(0,'c');
       expect(temperatura.tipo).to.be.a('string');
     });
     
-    it("Celsius tiene que tener el attributo tipo igual a 'c'", function() {
+    it("Instancia de Celsius() - Celsius tiene que tener el attributo tipo igual a 'c'", function() {
       var temperatura = new Celsius(0);
       temperatura.should.have.property('tipo').equal('c');
     });
   
-    it("Farenheit tiene que tener el atributo tipo igual a 'f'", function() {
+    it("Instancia de Farenheit() - Farenheit tiene que tener el atributo tipo igual a 'f'", function() {
       var temperatura = new Farenheit(0);
       temperatura.should.have.property('tipo').equal('f');
+    });
+    
+     it("Instancia de Distancia() - Distancia debe recibir dos parametros", function() {
+      var distancia = new Distancia(2,'m');
+      expect(distancia.valor).to.equal(2);
+      expect(distancia.tipo).to.equal('m');
+    });
+    
+     it("Instancia de Metros() - El atributo tipo de Metro tiene que ser 'm'", function() {
+      var metro = new Metros(0);
+      metro.should.have.property('tipo').equal('m');
     });
     
   });
   
   describe("Metodos",function() {
     
-    it("GetMedida() debe devolvernos la medida 2C",function(){
+    it("Metodo: getMedida() - 2c debe devolvernos la medida 2c",function(){
       var temperatura = new Temperatura(2,'c');
       expect(temperatura.getMedida()).to.equal("2c");
     });
     
-    it("Si pasamos 0.0 Celsius debemos obtener 32 Farenheit",function(){
+    it("Metodo: toFarenheit() - 0.0 Celsius debe ser 32 Farenheit",function(){
       var temperatura = new Celsius(0);
       temperatura.toFarenheit().should.equal(new Temperatura(32,'f').valor);
     });
     
-    it("Si pasamos 32 Farenheit debemos obtener 0 Celsius",function(){
+    it("Metodo: toCelsius() - 32 Farenheit debe ser 0 Celsius",function(){
       var temperatura = new Farenheit(32.0);
       temperatura.toCelsius().should.equal(new Temperatura(0,'c').valor);
     });
     
+    it("Metodo: toCentimetros() - 1 Metro deben ser 100 Centimetros",function(){
+      var metro = new Metros(1);
+      metro.toCentimetros().should.equal(new Distancia(100,'m').valor);
+    });
+    
+  });
+  
+  describe("I/O Formulario HTML", function() {
+    
+    it("Formulario HTML - Si el input tiene 32f nos debe devolver '0.00 Celsius'",function(){
+      convert.value = "32F";
+      convertir();
+      expect(converted.innerHTML).to.equal("0.00 Celsius");
+    });
+      
+    it("Formulario HTML - Si el input no contiene nada, el span tampoco debe contener nada",function(){
+      convert.value = "";
+      convertir();
+      expect(converted.innerHTML).to.equal("");
+    });
+    
+    it("Formulario HTML - Si la medida pasada no es reconocida debe crear una instancia de Medida",function() {
+      convert.value = "20v";
+      convertir();
+      expect(converted.innerHTML).to.equal("20v");
+    });
+    
+    it("Formulario HTML - Si la medida pasada es celsius, debe crear una instanca de Celsius", function() {
+      convert.value = "20C";
+      convertir();
+      expect(converted.innerHTML).to.equal("68.00 Farenheit");
+    });
+    
+    it("Formulario HTML - Si la medida pasada es metro, debe crear una instanca de Metros",function() {
+      convert.value = "2m";
+      convertir();
+      expect(converted.innerHTML).to.equal("200.00 Centimetros")
+    })
+      
   });
   
   describe("Logs", function() {
     
-    it("Debe haber un log para el metodo GetMedida()", function() {
+    it("Console.log() - Debe haber un log para el metodo GetMedida()", function() {
       var log = (new Temperatura(2,'c').getMedida());
       sinon.assert.notCalled(console.error);
       sinon.assert.calledOnce(console.log);
       sinon.assert.calledWithExactly(console.log, "La medida es: 2c");
     });
     
-    it("Debe haber un log para el metodo toFarenheit()", function() {
+    it("Console.log() - Debe haber un log para el metodo toFarenheit()", function() {
       var log = (new Celsius(0).toFarenheit());
       sinon.assert.notCalled(console.error);
       sinon.assert.calledOnce(console.log);
       sinon.assert.calledWithExactly(console.log, "Dato convertido 32 Farenheit");
     });
     
-    it("Debe haber un log para el metodo toCelsius()", function() {
+    it("Console.log() - Debe haber un log para el metodo toCelsius()", function() {
       var log = (new Farenheit(32).toCelsius());
       sinon.assert.notCalled(console.error);
       sinon.assert.calledOnce(console.log);
       sinon.assert.calledWithExactly(console.log, "Dato convertido 0 Celsius");
-    });
-    
-    it("Debe haber log en el metodo convertir()", function() {
-      var input = document.getElementById('convert').value;
-      input = "20C";
-      convertir();
-      sinon.assert.notCalled(console.error);
-      sinon.assert.calledOnce(console.log);
-      sinon.assert.calledWithExactly(console.log, "Valor: 20, Tipo: c");
     });
     
   });
